f41a2b4576ad7850819af6c622565275585b9684
tuanrobo <tuanrobo@gmail.com>
2019-12-27 09:06:44 +0700

    Updates

---
+++ precache-manifest.739f9914211e8a968b49fceaec7d74f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74c9804cbc2e4b406d8355481799c52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd748fe604c3c720944f",
+    "url": "/static/css/2.e975ae8e.chunk.css"
+  },
+  {
+    "revision": "60a712b8f9c6eca4eb6f",
+    "url": "/static/css/main.bb6990a7.chunk.css"
+  },
+  {
+    "revision": "bd748fe604c3c720944f",
+    "url": "/static/js/2.55061203.chunk.js"
+  },
+  {
+    "revision": "f58d20fb3c18b0082c6bfa6f78af043f",
+    "url": "/static/js/2.55061203.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60a712b8f9c6eca4eb6f",
+    "url": "/static/js/main.1711f061.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  }
+]);
